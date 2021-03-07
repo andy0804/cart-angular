7197992f725d3f827c9dfdf4e22c64b869df2901
Ananth <ananth.kumbla@gmail.com>
2021-03-07 22:40:52 +0530

    adding some mixins

--- cart-app/src/app/app.module.ts
+++ cart-app/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { ItemQuantityComponent } from './common/widgets/item-quantity/item-quantity.component';
 import { HeaderComponent } from './common/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ApiService } from './services/api.service';
@@ -13,12 +12,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    ItemQuantityComponent,
-    HeaderComponent,
-  ],
+  declarations: [AppComponent, HomeComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
