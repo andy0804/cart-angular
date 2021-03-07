98d49d2fc7a0bd859483d788a34e01332d9d44e0
Ananth <ananth.kumbla@gmail.com>
2021-03-07 22:11:25 +0530

    some changes

--- cart-app/src/app/services/shared.service.ts
+++ cart-app/src/app/services/shared.service.ts
@@ -5,10 +5,17 @@ import { Subject } from 'rxjs';
 export class SharedService {
   public notifySpinner$: any = new Subject<any>();
   public notifyRoute$: any = new Subject<any>();
-
+  public wishlist: any = [];
   constructor() {}
 
   public notifySpinner(obj: any): void {
     this.notifySpinner$.next(obj);
+  }
+
+  public setWishList(data: any) {
+    this.wishlist.push(data);
+  }
+  public getWishList() {
+    return this.wishlist;
   }
 }
