a9c6defd9aba895151805408247c9f48223e668a
Ananth <ananth.kumbla@gmail.com>
2021-03-07 22:10:52 +0530

    wish list changes

--- cart-app/src/app/shopping/product-detail/product-detail.component.ts
+++ cart-app/src/app/shopping/product-detail/product-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,7 +8,52 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   @Input() cartDetails: any;
-  constructor() {}
+  wishList: any = [];
+  constructor(
+    private sharedService: SharedService,
+    private elRef: ElementRef
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.wishList = this.sharedService.getWishList();
+  }
+
+  toggleWishList(event: any, cartDetails: any) {
+    const currentElement = event.target.id;
+    if (
+      this.elRef.nativeElement
+        .querySelector(`#${currentElement}`)
+        .classList.contains('fa-heart-o')
+    ) {
+      this.addToWishList(currentElement, cartDetails);
+    } else {
+      this.removeWishList(currentElement, cartDetails);
+    }
+  }
+
+  addToWishList(currentElement: any, cartDetails: any) {
+    this.elRef.nativeElement
+      .querySelector(`#${currentElement}`)
+      .classList.remove('fa-heart-o');
+    this.elRef.nativeElement
+      .querySelector(`#${currentElement}`)
+      .classList.add('fa-heart');
+    let wList = this.sharedService.getWishList();
+    if (!wList.find((i: any) => i.itemId === cartDetails.itemId)) {
+      this.sharedService.setWishList(cartDetails);
+    }
+  }
+
+  removeWishList(currentElement: any, cartDetails: any) {
+    this.elRef.nativeElement
+      .querySelector(`#${currentElement}`)
+      .classList.remove('fa-heart');
+    this.elRef.nativeElement
+      .querySelector(`#${currentElement}`)
+      .classList.add('fa-heart-o');
+
+    let wList = this.sharedService.getWishList();
+    wList = wList.filter((i: any) => i.itemId !== cartDetails.itemId);
+    this.sharedService.setWishList(wList);
+  }
 }
